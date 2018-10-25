c3c7f189780d2e29aa89bac14e82ce3005494b26
Dorian Haglund <dorian.haglund@gmail.com>
2018-10-25 11:56:57 +0200

    put code into a main function (so it won't be called during unit tests)

--- cgi/test.py
+++ cgi/test.py
@@ -7,28 +7,28 @@ cgitb.enable()
 
 import controller
 
-print("Content-type: text/html")
-print("")
+def main():
+    print("Content-type: text/html")
+    print("")
 
-print("""
-<html>
-<body>
-    Lore ipsum
-""")
+    print("""
+    <html>
+    <body>
+        Lore ipsum
+    """)
 
-form = cgi.FieldStorage()
-keywords = form["search"].value.split()
+    form = cgi.FieldStorage()
+    keywords = form["search"].value.split()
 
-comic_ids = get_ids(keywords)
+    comic_ids = get_ids(keywords)
 
-print("""
-</body>
-</html>
-""")
-
+    print("""
+    </body>
+    </html>
+    """)
 
 if __name__ == "__main__":
-    unittest.main()
+    main()
 
 # vim: set ft=python
 
