1ea4443661b4d61631cde75ba84729dd8aed18e5
Dorian Haglund <dorian.haglund@gmail.com>
2018-10-25 10:19:56 +0200

    add a basic controler filer which allow to retrieve comics id given a keyword

---
+++ cgi/controler.py
@@ -0,0 +1,42 @@
+import unittest
+
+def get_ids(keywords, database):
+    ans = {}
+    for keyword in keywords:
+        for entry in database:
+            if keyword in database[entry]:
+                if entry not in ans.keys():
+                    ans[entry] = [keyword]
+                else:
+                    ans[entry].append(keyword)
+
+    return ans
+
+class TestGetIds(unittest.TestCase):
+
+    def test_empty(self):
+        self.assertEqual({}, get_ids([''], {}))
+
+    def test_keyword_doesnt_match(self):
+        self.assertEqual({}, get_ids(['keywordNotFound'], {}))
+
+    def test_basic_match(self):
+        self.assertEqual({'comic-id' : ['my-keyword']}, 
+                get_ids(['my-keyword'], {'comic-id' : ['my-keyword']}))
+
+    def test_several_matches(self):
+        self.assertEqual(
+                {'comic-id1' : ['keyword2'], 'comic-id2' : ['keyword1']},
+                get_ids(['keyword1', 'keyword2'], 
+                    { 'comic-id1' : 'keyword2', 'comic-id2' : 'keyword1' }))
+
+    def test_one_keyword_matches(self):
+        self.assertEqual(
+                {'comic-id1' : ['keyword2']},
+                get_ids(['keyword1', 'keyword2'],
+                    { 'comic-id1' : 'keyword2' }))
+
+
+
+if __name__ == "__main__":
+    unittest.main()
