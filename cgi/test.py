b7c84205f1dcc80c3b21b48b94853b7cca236e25
Dorian Haglund <dorian.haglund@gmail.com>
2018-10-26 16:25:34 +0200

    return a html list, split the read file before giving it to get_all_comics

--- cgi/test.py
+++ cgi/test.py
@@ -4,22 +4,32 @@ import unittest
 import cgi
 import cgitb; 
 cgitb.enable()
+import os
 
 import controller
 
 def get_html():
     form = cgi.FieldStorage()
     keywords = form["search"].value.split()
+    res = '<ul>'
+    for comic in get_comic_list(keywords):
+        res += "<li>" + get_comic_html(comic) + "</li>"
+    res += "</ul>"
+    return res
+
+def get_comic_html(comic):
+    return "<img src='/img/" + comic + " alt='" + comic + "'>"
 
 def get_comic_list(keywords):
     db_file = open('/home/dorian/geeky/data/db.txt')
-    db = controller.get_all_comics(db_file)
+    db = controller.get_all_comics(db_file.read().split(os.linesep))
     db_file.close()
     return controller.get_ids(keywords, db)
 
 class TestGetComicList(unittest.TestCase):
     def test_crash(self):
-        get_comic_list('foo')
+        self.assertEqual({}, get_comic_list('foo'))
+        self.assertTrue('smbc-001' in get_comic_list(['alligator']))
 
 def main():
     print("Content-type: text/html")
