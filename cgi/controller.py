e33b1758e046cf69a84b87af163092933964e2b4
Dorian Haglund <dorian.haglund@gmail.com>
2018-10-26 12:17:57 +0200

    add functions to parse comics' database and to populate a id -> (keywords+) dictionnary

--- cgi/controller.py
+++ cgi/controller.py
@@ -36,3 +36,60 @@ class TestGetIds(unittest.TestCase):
                 get_ids(['keyword1', 'keyword2'],
                     { 'comic-id1' : 'keyword2' }))
 
+def parse_comics_db_line(line):
+    if line == '':
+        return None
+
+    first_split = line.split(':')
+
+    if len(first_split) != 2:
+        return None
+
+    id_ = first_split[0]
+    keywords = first_split[1].split(',')
+    return id_,keywords
+
+
+class TestParseDbLine(unittest.TestCase):
+    def test_parse_empty(self):
+        self.assertEqual(None, parse_comics_db_line(""))
+
+    def test_parse_gibberish(self):
+        self.assertEqual(None, parse_comics_db_line("not a valid syntax"))
+
+    def test_parse_minimal(self):
+        self.assertEqual(('comic_id', ['keyword']), 
+                parse_comics_db_line('comic_id:keyword'))
+
+    def test_parse_multiple_keywords(self):
+        self.assertEqual(('comic_id', ['keyword1', 'keyword2']), 
+                parse_comics_db_line('comic_id:keyword1,keyword2'))
+
+
+
+def get_all_comics(lines):
+    ans = {}
+    for line in lines:
+        pair = parse_comics_db_line(line)
+        if pair == None:
+            continue
+
+        if pair[0] not in ans:
+            ans[pair[0]] = pair[1]
+        else:
+            for kw in pair[1]:
+                if kw not in ans[pair[0]]:
+                    ans[pair[0]].append(kw)
+
+    return ans
+
+class TestInsertDbLines(unittest.TestCase):
+    def test_double_insertion(self):
+        self.assertEqual({'same_comic_id' : ['same_keyword']},
+                get_all_comics(['same_comic_id:same_keyword',
+                'same_comic_id:same_keyword'
+                ]))
+
+    def test_multiple_occurence(self):
+        self.assertEqual({'comic_id' : ['keyword1', 'keyword2']},
+                get_all_comics(['comic_id:keyword1', 'comic_id:keyword2']))
