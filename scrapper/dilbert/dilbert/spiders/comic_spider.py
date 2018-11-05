b342dbdbca1d41937f19f534f9f5c89ff6867ef1
Dorian Haglund <dorian.haglund@gmail.com>
2018-11-05 12:34:36 +0100

    add a spider for dilbert scrapping

---
+++ scrapper/dilbert/dilbert/spiders/comic_spider.py
@@ -0,0 +1,28 @@
+import scrapy
+import shutil
+import tempfile
+import urllib.response
+
+class ComicsSpider(scrapy.Spider):
+    name = 'Comics Spider'
+    start_urls = [ 'http://dilbert.com/strip/2018-11-05' ]
+    counter = 0
+
+    def parse(self, response):
+        if self.counter >= 10:
+            return None
+
+        img_url = response.css('img.img-comic::attr(src)').extract_first()
+        img = urllib.request.urlopen(img_url)
+        dst_file = open(self.getFileName(), 'wb')
+        shutil.copyfileobj(img, dst_file)
+
+        previous_img_link = response.css('div.nav-left a::attr(href)').extract_first()
+        if previous_img_link is not None:
+            self.counter += 1
+            yield response.follow(previous_img_link, self.parse)
+        else:
+            return None
+
+    def getFileName(self):
+        return 'dilbert-{}.gif'.format(self.counter)
