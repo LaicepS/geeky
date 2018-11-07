7aa4e5c8c50476e348b2f4814fa3869123f8dd69
Dorian Haglund <dorian.haglund@gmail.com>
2018-11-07 15:31:17 +0100

    remove duplicated words from words extracted from comics

--- ocr/db_gen.py
+++ ocr/db_gen.py
@@ -24,13 +24,23 @@ class ExtractWordsTests(unittest.TestCase):
     def testSimpleFile(self):
         self.assertEqual(['FOO'], extract_words('test.gif'))
 
+def deduplicate(word_list):
+    return set(word_list)
+
+class TestDeduplicate(unittest.TestCase):
+    def test_empty_deduplicatoin(self):
+        self.assertEqual(set(), deduplicate([]))
+
+    def test_basic_deduplicate(self):
+        self.assertEqual({'foo', 'bar'}, deduplicate({'foo', 'bar', 'foo'}))
+
 
 def gen_dictionary(dir):
     res = {}
     for dirpath, dirnames, filenames in os.walk(dir.name, followlinks=False):
         gif_filenames = filter(lambda f: f.lower().endswith('.gif'), filenames)
         for gif in gif_filenames:
-            res[gif] = extract_words(os.path.join(dirpath, gif))
+            res[gif] = deduplicate(extract_words(os.path.join(dirpath, gif)))
 
     return res
 
@@ -50,15 +60,15 @@ class testDbGen(unittest.TestCase):
 
         result = gen_dictionary(self.test_dir)
         self.assertIn('file1.gif', result)
-        self.assertEqual([], result['file1.gif'])
+        self.assertEqual(set(), result['file1.gif'])
         self.assertIn('file2.gif', result)
-        self.assertEqual([], result['file2.gif'])
+        self.assertEqual(set(), result['file2.gif'])
 
     def testBasicImg(self):
         shutil.copy('test.gif', self.test_dir.name)
         res = gen_dictionary(self.test_dir)
         self.assertIn('test.gif', res)
-        self.assertEqual(['FOO'], res['test.gif'])
+        self.assertEqual({'FOO'}, res['test.gif'])
 
     def tearDown(self):
         self.test_dir.cleanup()
