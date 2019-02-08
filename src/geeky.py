68b8144e2b87b7b0f8c43df0a912952caf56075d
Dorian Haglund <dorian.haglund@gmail.com>
2019-02-08 15:07:00 +0100

    add a basic implem for the binary

--- src/geeky.py
+++ src/geeky.py
@@ -1,12 +1,34 @@
 import argparse
 
+import controller
+import MySQLdb as mdb
+
 def parseArgs():
     parser = argparse.ArgumentParser(description='Search for a comic based on keywords.')
-    parser.add_argument('strings', metavar='keyword', nargs='+', help='Keyword to look for')
+    parser.add_argument('keywords', metavar='keyword', nargs='+', help='Keyword to look for')
     args = parser.parse_args()
+    return args
 
 def main():
-    parseArgs()
+    args = parseArgs()
+    kw = args.keywords
+
+    try:
+        con = mdb.connect('localhost', 'dorian', 'moijemon', 'geeky')
+        cursor = con.cursor()
+        cursor.execute('select * from comics')
+        rows = cursor.fetchall()
+
+        for row in rows:
+            if kw[0] in row[2]:
+                print(row)
+
+    except e:
+       stderr.print(e, file=stderr)
+
+
+    return args
+
 
 if __name__ == '__main__':
     main()
