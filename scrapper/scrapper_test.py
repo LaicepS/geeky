b1d02a8d9a370ac207ad8fcc23f45bbad8794472
Dorian Haglund <dorian.haglund@gmail.com>
2019-02-13 17:36:14 +0100

    improve the spider testing by giving it a custom listener

--- scrapper/scrapper_test.py
+++ scrapper/scrapper_test.py
@@ -1,6 +1,26 @@
+import os
+
+import scrapy
+from scrapy.crawler import CrawlerProcess
+
 import unittest
 
-import dilbert.dilbert.spiders.comic_spider
+from  dilbert.dilbert.spiders.comic_spider import ComicsSpider
+
+class SpiderListener:
+    testObject = None
+
+    def __init__(self, testObject):
+        self.testObject = testObject
+
+    def onImg(self, img_file):
+        self.testObject.assertTrue(img_file.readable())
+
 
 class ComicSpiderTest(unittest.TestCase):
-    pass
+    def test_scrapper(self):
+        process = CrawlerProcess({'USER_AGENT' : 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
+            'LOG_ENABLED' : False})
+        spiderListener = SpiderListener(self)
+        process.crawl(ComicsSpider, spiderListener)
+        process.start()
