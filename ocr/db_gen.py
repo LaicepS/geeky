63c889b44c941bfb4e5c0e1ff97da0c53b12ebac
Dorian Haglund <dorian.haglund@gmail.com>
2019-02-20 16:57:51 +0100

    remove the code from extract_word: that's not how it is done anymore. change gen_dictionnary so it comes from some DB rows instead of files

--- ocr/db_gen.py
+++ ocr/db_gen.py
@@ -3,27 +3,6 @@ import unittest
 import shutil
 import tempfile
 
-def extract_words(img_file):
-    res = []
-    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-        tmp_file.close()
-        cmd = '2> /dev/null tesseract {} stdout > {}'.format(img_file, tmp_file.name)
-        os.system(cmd)
-        f = open(tmp_file.name)
-        for l in f.readlines():
-            res += l.strip().split()
-        f.close()
-    return res
-
-class ExtractWordsTests(unittest.TestCase):
-    # test empty file should return nothing
-    def testEmptyFile(self):
-        with tempfile.NamedTemporaryFile() as empty_file:
-            self.assertEqual([], extract_words(empty_file.name))
-
-    def testSimpleFile(self):
-        self.assertEqual(['FOO'], extract_words('test.gif'))
-
 def deduplicate(word_list):
     return set(word_list)
 
@@ -35,42 +14,28 @@ class TestDeduplicate(unittest.TestCase):
         self.assertEqual({'foo', 'bar'}, deduplicate({'foo', 'bar', 'foo'}))
 
 
-def gen_dictionary(dir):
+def gen_dictionary(rows):
     res = {}
-    for dirpath, dirnames, filenames in os.walk(dir.name, followlinks=False):
-        gif_filenames = filter(lambda f: f.lower().endswith('.gif'), filenames)
-        for gif in gif_filenames:
-            res[gif] = deduplicate(extract_words(os.path.join(dirpath, gif)))
+    for row in rows:
+        for word in row[2].split():
+            if word not in res:
+                res[word] = [ row[0] ]
+            else:
+                res[word].append(row[0])
 
     return res
 
 class testDbGen(unittest.TestCase):
-    def setUp(self):
-        self.test_dir = tempfile.TemporaryDirectory()
+    def testSmallDb(self):
+        self.assertEqual({ 'funny': [123], 'dilbert' : [123], 'joke' : [123] },
+                gen_dictionary([(123, 'dilbert', 'funny dilbert joke')]))
 
-    def testEmptyDir(self):
-        self.assertEqual({}, gen_dictionary(self.test_dir))
-
-    def testEmptyFiles(self):
-        empty_files = ['file1.gif', 'file2.gif']
-        for file_name in empty_files:
-            file_path = os.path.join(self.test_dir.name, file_name)
-            f = open(file_path, "wb")
-            f.write(''.encode())
-            f.close()
-
-        result = gen_dictionary(self.test_dir)
-
-        for file_name in empty_files:
-            self.assertIn(file_name, result)
-            self.assertEqual(set(), result[file_name])
-
-    def testBasicImg(self):
-        shutil.copy('test.gif', self.test_dir.name)
-        res = gen_dictionary(self.test_dir)
-        self.assertIn('test.gif', res)
-        self.assertEqual({'FOO'}, res['test.gif'])
-
-    def tearDown(self):
-        self.test_dir.cleanup()
+    def testTwoRowsDB(self):
+        self.assertEqual({ 'funny': [123, 234],
+            'dilbert' : [123],
+            'joke' : [123, 234],
+            'another' : [234]
+            }, gen_dictionary([
+                (123, 'dilbert', 'funny dilbert joke'),
+                (234, 'dilbert', 'another funny joke')]))
 
