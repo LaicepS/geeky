11882788c65c701598f0b6d7b4d1516bf55b2042
Dorian Haglund <dorian.haglund@gmail.com>
2019-10-12 19:30:11 +0200

    add an origin field and a __str__ method

--- api_server/search/models.py
+++ api_server/search/models.py
@@ -3,3 +3,7 @@ from django.db import models
 class Comic(models.Model):
     url = models.URLField(max_length=400)
     keywords = models.CharField(max_length=400)
+    origin = models.CharField(max_length=200, default=None)
+
+    def __str__(self):
+        return "{}: {}".format(self.origin, self.keywords)
