4fd53607ed2b0108399791b3be2859a9942cca90
Dorian Haglund <dorian.haglund@gmail.com>
2019-10-13 13:15:05 +0200

    first end to end test using django

--- api_server/search/tests.py
+++ api_server/search/tests.py
@@ -1,9 +1,20 @@
 import json
 
+from search.models import Comic
 from django.test import TestCase, Client
 
 class End2EndTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Comic.objects.create(
+            url = 'http://toto.com',
+            keywords = 'foo bar joke',
+            origin = 'Doiran',
+        )
+
     def test_basic_end2end_test(self):
         client = Client()
-        get_request = client.get('/search/')
+        get_request = client.get('/search/', {'keywords' : 'foo'})
+        assert get_request.status_code == 200
         response = json.loads(get_request.getvalue())
+        assert {'origin': 'Doiran', 'url': 'http://toto.com'} == response
--- api_server/search/views.py
+++ api_server/search/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 
 import json
 
+from search.models import Comic
+
 def index(request):
-    d = { "toto" : "tata" }
-    return HttpResponse(json.dumps(d))
+    request_params = request.GET
+    query = Comic.objects.filter(keywords__contains=request_params['keywords'])
+    comic = get_object_or_404(query)
+    return HttpResponse(json.dumps({'origin': comic.origin, 'url': comic.url}))
