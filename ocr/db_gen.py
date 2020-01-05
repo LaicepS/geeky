78adeab5717cdae8ef1f8250fcfb21e59c9aba2f
Dorian Haglund <dorian.haglund.ext@mappy.com>
2020-01-05 16:22:27 +0100

    remove dead code

--- ocr/db_gen.py
+++ ocr/db_gen.py
@@ -4,18 +4,6 @@ import shutil
 import tempfile
 
 
-def deduplicate(word_list):
-    return set(word_list)
-
-
-class TestDeduplicate(unittest.TestCase):
-    def test_empty_deduplicatoin(self):
-        self.assertEqual(set(), deduplicate([]))
-
-    def test_basic_deduplicate(self):
-        self.assertEqual({"foo", "bar"}, deduplicate({"foo", "bar", "foo"}))
-
-
 def gen_dictionary(rows):
     res = {}
     for row in rows:
