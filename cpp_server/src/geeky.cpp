fc6ca8fe514d9257f5d05b2290c96b0153511ad8
Dorian Haglund <dorian.haglund@gmail.com>
2020-05-12 16:40:08 +0200

    t

--- cpp_server/src/geeky.cpp
+++ cpp_server/src/geeky.cpp
@@ -1,21 +1,22 @@
+#include <algorithm>
+#include <cstdlib>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <thread>
+#include <vector>
+
 #include <boost/asio/detail/throw_error.hpp>
+#include <boost/asio/dispatch.hpp>
 #include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/strand.hpp>
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
 #include <boost/beast/http/string_body.hpp>
 #include <boost/beast/version.hpp>
 #include <boost/config.hpp>
 #include <boost/system/error_code.hpp>
-#include <cstdlib>
-#include <iostream>
-#include <memory>
-#include <string>
-#include <thread>
-
-namespace beast = boost::beast;    // from <boost/beast.hpp>
-namespace http = beast::http;      // from <boost/beast/http.hpp>
-namespace net = boost::asio;       // from <boost/asio.hpp>
-using tcp = boost::asio::ip::tcp;  // from <boost/asio/ip/tcp.hpp>
 
 using namespace std;
 
@@ -26,102 +27,11 @@ using namespace boost::asio::ip;
 using namespace boost::beast;
 using port = unsigned short;
 
-#include <algorithm>
-#include <boost/asio/dispatch.hpp>
-#include <boost/asio/strand.hpp>
-#include <boost/beast/core.hpp>
-#include <boost/beast/http.hpp>
-#include <boost/beast/version.hpp>
-#include <boost/config.hpp>
-#include <cstdlib>
-#include <functional>
-#include <iostream>
-#include <memory>
-#include <string>
-#include <thread>
-#include <vector>
-
 namespace beast = boost::beast;    // from <boost/beast.hpp>
 namespace http = beast::http;      // from <boost/beast/http.hpp>
 namespace net = boost::asio;       // from <boost/asio.hpp>
 using tcp = boost::asio::ip::tcp;  // from <boost/asio/ip/tcp.hpp>
 
-// Return a reasonable mime type based on the extension of a file.
-beast::string_view mime_type(beast::string_view path) {
-  using beast::iequals;
-  auto const ext = [&path] {
-    auto const pos = path.rfind(".");
-    if (pos == beast::string_view::npos)
-      return beast::string_view{};
-    return path.substr(pos);
-  }();
-  if (iequals(ext, ".htm"))
-    return "text/html";
-  if (iequals(ext, ".html"))
-    return "text/html";
-  if (iequals(ext, ".php"))
-    return "text/html";
-  if (iequals(ext, ".css"))
-    return "text/css";
-  if (iequals(ext, ".txt"))
-    return "text/plain";
-  if (iequals(ext, ".js"))
-    return "application/javascript";
-  if (iequals(ext, ".json"))
-    return "application/json";
-  if (iequals(ext, ".xml"))
-    return "application/xml";
-  if (iequals(ext, ".swf"))
-    return "application/x-shockwave-flash";
-  if (iequals(ext, ".flv"))
-    return "video/x-flv";
-  if (iequals(ext, ".png"))
-    return "image/png";
-  if (iequals(ext, ".jpe"))
-    return "image/jpeg";
-  if (iequals(ext, ".jpeg"))
-    return "image/jpeg";
-  if (iequals(ext, ".jpg"))
-    return "image/jpeg";
-  if (iequals(ext, ".gif"))
-    return "image/gif";
-  if (iequals(ext, ".bmp"))
-    return "image/bmp";
-  if (iequals(ext, ".ico"))
-    return "image/vnd.microsoft.icon";
-  if (iequals(ext, ".tiff"))
-    return "image/tiff";
-  if (iequals(ext, ".tif"))
-    return "image/tiff";
-  if (iequals(ext, ".svg"))
-    return "image/svg+xml";
-  if (iequals(ext, ".svgz"))
-    return "image/svg+xml";
-  return "application/text";
-}
-
-// Append an HTTP rel-path to a local filesystem path.
-// The returned path is normalized for the platform.
-std::string path_cat(beast::string_view base, beast::string_view path) {
-  if (base.empty())
-    return std::string(path);
-  std::string result(base);
-#ifdef BOOST_MSVC
-  char constexpr path_separator = '\\';
-  if (result.back() == path_separator)
-    result.resize(result.size() - 1);
-  result.append(path.data(), path.size());
-  for (auto& c : result)
-    if (c == '/')
-      c = path_separator;
-#else
-  char constexpr path_separator = '/';
-  if (result.back() == path_separator)
-    result.resize(result.size() - 1);
-  result.append(path.data(), path.size());
-#endif
-  return result;
-}
 
 // This function produces an HTTP response for the given
 // request. The type of the response object depends on the
